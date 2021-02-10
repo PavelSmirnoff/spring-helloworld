54a77c2d185486562d07eb936a798f1a16ee35b5
Pavel Smirnov <smirnov@0x55.ru>
2021-02-10 18:32:29 +0300

    Add bean Cat and equals bean

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,15 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean.getMessage());
+
+        Cat cat = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getName());
+        System.out.println(cat.getName());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean.equals(bean1));
+        System.out.println(cat.equals(cat1));
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,4 +11,12 @@ public class AppConfig {
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
+    @Bean (name="cat")
+    @Scope(value = "prototype")
+    public Cat getNameCat() {
+        Cat cat = new Cat();
+        cat.setName("Мурзик");
+        return cat;
+    }
 }
